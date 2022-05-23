cb3707b9648293b3831cc9f75b091fc5924ba658
Islambek Brgaliyev <66675271+isok-atyrau@users.noreply.github.com>
2022-05-23 21:16:36 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log(42)
+
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    document.querySelector('.nav__list-holder').classList.toggle('nav__list-holder--visible');
+}
